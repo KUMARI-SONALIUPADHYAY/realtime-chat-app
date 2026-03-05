8a94fc05eef7fef160da2005193a77a7c91cd054
KUMARI-SONALIUPADHYAY <upadhyaysonali2005@gmail.com>
2026-03-05 23:26:28 +0530

    fixed realtime chat deployment

--- client/chat.js
+++ client/chat.js
@@ -1,97 +1,40 @@
-const socket = io("http://localhost:3000");
+const socket = io();
 
-const messages = document.getElementById("messages");
-const messageInput = document.getElementById("messageInput");
-const sendBtn = document.getElementById("sendBtn");
-const typingDiv = document.getElementById("typing");
-const usersList = document.getElementById("usersList");
-const onlineCount = document.getElementById("onlineCount");
-const msgSound = document.getElementById("msgSound");
-
-const username = prompt("Enter your name");
+let username = prompt("Enter your name");
 
 socket.emit("join", username);
 
-sendBtn.addEventListener("click", sendMessage);
-
-messageInput.addEventListener("keypress",(e)=>{
-    if(e.key === "Enter"){
-        sendMessage();
-    }
-});
-
-messageInput.addEventListener("input",()=>{
-    socket.emit("typing", username);
-});
+const messageInput = document.getElementById("messageInput");
+const messages = document.getElementById("messages");
+const userList = document.getElementById("userList");
+const onlineCount = document.getElementById("onlineCount");
+const typing = document.getElementById("typing");
 
 function sendMessage(){
 
-    const message = messageInput.value;
+    const msg = messageInput.value;
 
-    if(message.trim()==="") return;
+    if(msg === "") return;
 
     const data = {
-        user: username,
-        message: message,
+        username: username,
+        message: msg,
         time: new Date().toLocaleTimeString()
     };
 
     socket.emit("sendMessage", data);
 
-    messageInput.value="";
+    messageInput.value = "";
 }
 
 socket.on("receiveMessage",(data)=>{
 
-    msgSound.play();
-
     const div = document.createElement("div");
-    div.classList.add("message");
 
-    const avatar = document.createElement("div");
-    avatar.classList.add("avatar");
-    avatar.innerText = data.user.charAt(0).toUpperCase();
-
-    const bubble = document.createElement("div");
-    bubble.classList.add("bubble");
-
-    bubble.innerHTML = `
-        <div class="name">${data.user}</div>
-        <div>${data.message}</div>
-        <div class="time">${data.time}</div>
-    `;
-
-    if(data.user === username){
-        div.classList.add("own");
-    }
-
-    div.appendChild(avatar);
-    div.appendChild(bubble);
-
-    messages.appendChild(div);
-
-    messages.scrollTo({
-        top: messages.scrollHeight,
-        behavior: "smooth"
-    });
-
-});
-
-socket.on("typing",(user)=>{
-
-    typingDiv.innerText = user + " is typing...";
-
-    setTimeout(()=>{
-        typingDiv.innerText="";
-    },2000);
-
-});
-
-socket.on("systemMessage",(msg)=>{
-
-    const div = document.createElement("div");
-    div.classList.add("system");
-    div.innerText = msg;
+    div.innerHTML =
+    "<b>"+data.username+"</b>: "
+    +data.message+
+    " <small>"+data.time+"</small>";
 
     messages.appendChild(div);
 
@@ -99,17 +42,26 @@ socket.on("systemMessage",(msg)=>{
 
 socket.on("onlineUsers",(users)=>{
 
-    onlineCount.innerText = users.length;
-
-    usersList.innerHTML="";
+    userList.innerHTML = "";
 
     users.forEach(user=>{
-
         const li = document.createElement("li");
-        li.innerText = user;
-
-        usersList.appendChild(li);
-
+        li.textContent = user;
+        userList.appendChild(li);
     });
 
+    onlineCount.innerText = "Online Users: " + users.length;
+
+});
+
+messageInput.addEventListener("input",()=>{
+    socket.emit("typing",username);
+});
+
+socket.on("typing",(user)=>{
+    typing.innerText = user + " is typing...";
+
+    setTimeout(()=>{
+        typing.innerText="";
+    },2000);
 });
--- server/server.js
+++ server/server.js
@@ -7,15 +7,13 @@ const path = require("path");
 const Message = require("./models/Message");
 
 const app = express();
+const server = http.createServer(app);
 
-// serve frontend
 app.use(express.static(path.join(__dirname, "../client")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/chat.html"));
 });
-
-const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
@@ -23,62 +21,59 @@ const io = new Server(server, {
   }
 });
 
-// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
 .then(()=> console.log("MongoDB connected"))
 .catch(err => console.log(err));
 
 let users = {};
 
-io.on("connection", (socket) => {
+io.on("connection",(socket)=>{
 
-  console.log("User connected");
+    console.log("User connected");
 
-  socket.on("join", (username) => {
+    socket.on("join",(username)=>{
 
-    users[socket.id] = username;
+        users[socket.id] = username;
 
-    io.emit("onlineUsers", Object.values(users));
+        io.emit("onlineUsers", Object.values(users));
 
-    socket.broadcast.emit("systemMessage", username + " joined the chat");
+        socket.broadcast.emit("systemMessage", username + " joined the chat");
 
-  });
+    });
 
-  socket.on("sendMessage", async (data) => {
+    socket.on("sendMessage", async(data)=>{
 
-    const msg = new Message(data);
-    await msg.save();
+        const msg = new Message(data);
+        await msg.save();
 
-    io.emit("receiveMessage", data);
+        io.emit("receiveMessage", data);
 
-  });
+    });
 
-  socket.on("typing", (username) => {
+    socket.on("typing",(username)=>{
+        socket.broadcast.emit("typing", username);
+    });
 
-    socket.broadcast.emit("typing", username);
+    socket.on("disconnect",()=>{
 
-  });
+        const username = users[socket.id];
 
-  socket.on("disconnect", () => {
+        delete users[socket.id];
 
-    const username = users[socket.id];
+        io.emit("onlineUsers", Object.values(users));
 
-    delete users[socket.id];
+        if(username){
+            io.emit("systemMessage", username + " left the chat");
+        }
 
-    io.emit("onlineUsers", Object.values(users));
+        console.log("User disconnected");
 
-    if (username) {
-      io.emit("systemMessage", username + " left the chat");
-    }
-
-    console.log("User disconnected");
-
-  });
+    });
 
 });
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+    console.log("Server running on port " + PORT);
 });
